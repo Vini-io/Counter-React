a6ce7eb8e92ce3bf021e96313880fea679a1cd49
Vini-io <barbosavini2@gmail.com>
2023-02-09 21:37:14 -0300

    Counter React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(parseInt(localStorage.getItem("COUNT")))
+
+  useEffect(() => {
+    setCount(parseInt(localStorage.getItem("COUNT")))
+  }, [])
+
+  useEffect(() => {
+    let styleCount = document.querySelector(".count").classList
+    localStorage.setItem("COUNT", count)
+    document.title = count
+    if (count > 0) {
+      styleCount.add("positive")
+      styleCount.remove("negative", "neutral")
+    } else if (count == 0) {
+      styleCount.add("neutral")
+      styleCount.remove("positive", "negative")
+    } else {
+      styleCount.add("negative")
+      styleCount.remove("positive", "neutral")
+    }
+  })
+
+  function downCount() {
+    setCount(count - 1)
+  }
+
+  function reset() {
+    setCount(0)
+  }
+
+  function upCount() {
+    setCount(count + 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='contador'>
+      <span className='count'>{count}</span>
+      <div>
+        <button onClick={downCount}>-</button>
+        <button onClick={reset}>Reset</button>
+        <button onClick={upCount}>+</button>
+      </div>
     </div>
   );
 }
